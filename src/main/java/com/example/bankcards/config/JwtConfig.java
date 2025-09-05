7d48371287b29b0b895bc9d3cb85abe4ca610e64
Ageu <Borzhesh@yandex.ru>
2025-09-05 21:53:35 +0300

    добавил зависимости в конфигурации, создал классы в пакет конфигурации

---
+++ src/main/java/com/example/bankcards/config/JwtConfig.java
@@ -0,0 +1,36 @@
+package com.example.bankcards.config;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
+    @Bean
+    public SecretKey secretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public long getJwtExpiration() {
+        return jwtExpiration;
+    }
+
+    public long getRefreshExpiration() {
+        return refreshExpiration;
+    }
+}
